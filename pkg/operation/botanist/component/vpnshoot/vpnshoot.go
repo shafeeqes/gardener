ec673941632f8694dde296a8d81e8077393f92be
shafeeqes <32516833+shafeeqes@users.noreply.github.com>
2021-11-09 15:09:26 +0530

    add basic template for vpnshoot component

---
+++ pkg/operation/botanist/component/vpnshoot/vpnshoot.go
@@ -0,0 +1,84 @@
+// Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package vpnshoot
+
+import (
+	"context"
+	"time"
+
+	"github.com/gardener/gardener/pkg/client/kubernetes"
+	"github.com/gardener/gardener/pkg/operation/botanist/component"
+	"github.com/gardener/gardener/pkg/utils/managedresources"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	// ManagedResourceName is the name of the ManagedResource containing the resource specifications.
+	ManagedResourceName = "shoot-core-vpnshoot"
+)
+
+// New creates a new instance of DeployWaiter for vpnshoot
+func New(
+	client client.Client,
+	namespace string,
+	image string,
+) component.DeployWaiter {
+	return &vpnshoot{
+		client:    client,
+		namespace: namespace,
+		image:     image,
+	}
+}
+
+type vpnshoot struct {
+	client    client.Client
+	namespace string
+	image     string
+}
+
+func (v *vpnshoot) Deploy(ctx context.Context) error {
+	data, err := v.computeResourcesData()
+	if err != nil {
+		return err
+	}
+	return managedresources.CreateForShoot(ctx, v.client, v.namespace, ManagedResourceName, false, data)
+}
+
+func (v *vpnshoot) Destroy(ctx context.Context) error {
+	return managedresources.DeleteForShoot(ctx, v.client, v.namespace, ManagedResourceName)
+}
+
+var TimeoutWaitForManagedResource = 2 * time.Minute
+
+func (v *vpnshoot) Wait(ctx context.Context) error {
+	timeoutCtx, cancel := context.WithTimeout(ctx, TimeoutWaitForManagedResource)
+	defer cancel()
+
+	return managedresources.WaitUntilHealthy(timeoutCtx, v.client, v.namespace, ManagedResourceName)
+}
+
+func (v *vpnshoot) WaitCleanup(ctx context.Context) error {
+	timeoutCtx, cancel := context.WithTimeout(ctx, TimeoutWaitForManagedResource)
+	defer cancel()
+
+	return managedresources.WaitUntilDeleted(timeoutCtx, v.client, v.namespace, ManagedResourceName)
+}
+
+func (v *vpnshoot) computeResourcesData() (map[string][]byte, error) {
+	registry := managedresources.NewRegistry(kubernetes.ShootScheme, kubernetes.ShootCodec, kubernetes.ShootSerializer)
+
+	return registry.AddAllAndSerialize()
+}
